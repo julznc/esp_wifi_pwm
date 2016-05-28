4d6cb479788aef99f493e4631869c9753f1c67de
yus <yus.projectproto@gmail.com>
2016-05-28 20:01:10 +0800

    import library: https://github.com/neild001/SeekArc

--- app/src/main/java/com/ymsoftlabs/wifipwm/MainActivity.java
+++ app/src/main/java/com/ymsoftlabs/wifipwm/MainActivity.java
@@ -3,6 +3,8 @@ package com.ymsoftlabs.wifipwm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.triggertrap.seekarc.SeekArc;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
