1c9bd719882792dfe0fb03cdcae2a7eee375e99d
yus <yus.projectproto@gmail.com>
2016-05-28 22:01:22 +0800

    display seek values

--- app/src/main/java/com/ymsoftlabs/wifipwm/MainActivity.java
+++ app/src/main/java/com/ymsoftlabs/wifipwm/MainActivity.java
@@ -2,14 +2,64 @@ package com.ymsoftlabs.wifipwm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.triggertrap.seekarc.SeekArc;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekArc mGP0seekArc;
+    private SeekArc mGP2seekArc;
+    private TextView mGP0seekText;
+    private TextView mGP2seekText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGP0seekArc = (SeekArc) findViewById(R.id.gp0seekArc);
+        mGP2seekArc = (SeekArc) findViewById(R.id.gp2seekArc);
+        mGP0seekText = (TextView) findViewById(R.id.gp0seekText);
+        mGP2seekText = (TextView) findViewById(R.id.gp2seekText);
+
+        mGP0seekText.setText("0");
+        mGP2seekText.setText("0");
+
+        mGP0seekArc.setOnSeekArcChangeListener(new SeekArc.OnSeekArcChangeListener() {
+            @Override
+            public void onProgressChanged(SeekArc seekArc, int progress, boolean fromUser) {
+                mGP0seekText.setText(String.valueOf(progress));
+                //Log.d("wifipwm", String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekArc seekArc) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekArc seekArc) {
+
+            }
+        });
+
+        mGP2seekArc.setOnSeekArcChangeListener(new SeekArc.OnSeekArcChangeListener() {
+            @Override
+            public void onProgressChanged(SeekArc seekArc, int progress, boolean fromUser) {
+                mGP2seekText.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekArc seekArc) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekArc seekArc) {
+
+            }
+        });
     }
 }
